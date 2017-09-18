1bb1ec6bcb1bc951eb38ab08ff90eb73e6af3739
uknmr <y.kinmori@gmail.com>
2017-09-19 01:45:34 +0900

    :sparkles: 複数対象に対応する

--- src/runTest.js
+++ src/runTest.js
@@ -1,13 +1,16 @@
 import WebPagetest from './WebPagetest'
 import Utils from './Utils'
 
-global.runWebPagetest = (key, url) => {
+global.runWebPagetest = (key, ...targets) => {
   const wpt = new WebPagetest(key)
-  const testId = wpt.runTest(url)
-
   const activeSpreadSheet = SpreadsheetApp.getActiveSpreadsheet()
-  const sheet = activeSpreadSheet.getSheets()[0]
-  const lastARow = Utils.getLastRow(sheet, 'A')
 
-  sheet.getRange('A' + (lastARow + 1)).setValue(testId)
+  targets.forEach(({ url, sheetName, options }) => {
+    const testId = wpt.runTest(url, options)
+
+    const sheet = activeSpreadSheet.getSheetByName(sheetName)
+    const lastARow = Utils.getLastRow(sheet, 'A')
+
+    sheet.getRange('A' + (lastARow + 1)).setValue(testId)
+  })
 }
