58b76b6b58c4f25d71e36cf6f8881d1e661bfc47
uknmr <kanamori@mediba.jp>
2017-09-19 01:45:34 +0900

    :sparkles: 複数対象に対応する

--- src/getTestResults.js
+++ src/getTestResults.js
@@ -1,28 +1,30 @@
 import WebPagetest from './WebPagetest'
 import Utils from './Utils'
 
-global.getTestResults = testId => {
+global.getTestResults = () => {
   const activeSpreadSheet = SpreadsheetApp.getActiveSpreadsheet()
-  const sheet = activeSpreadSheet.getSheets()[0]
-  const lastTestIdRow = Utils.getLastRow(sheet, 'A')
-  const lastCompletedRow = Utils.getLastRow(sheet, 'B')
-  const testIds = sheet.getRange(`A${lastCompletedRow + 1}:A${lastTestIdRow}`)
-    .getValues()
-    .reduce((a, b) => a.concat(b))
 
-  if (!testIds.length) {
-    Logger.log('対象 testId はありませんでした')
-    return
-  }
+  activeSpreadSheet.getSheets().forEach(sheet => {
+    const lastTestIdRow = Utils.getLastRow(sheet, 'A')
+    const lastCompletedRow = Utils.getLastRow(sheet, 'B')
+    const testIds = sheet.getRange(`A${lastCompletedRow + 1}:A${lastTestIdRow}`)
+      .getValues()
+      .reduce((a, b) => a.concat(b))
 
-  const wpt = new WebPagetest()
-  const results = testIds.map(testId => wpt.getTestResults(testId))
+    if (!testIds.length) {
+      Logger.log('対象 testId はありませんでした')
+      return
+    }
 
-  const targetRange = sheet.getRange(
-    lastCompletedRow + 1,
-    2,
-    results.length,
-    results[0].length,
-  )
-  targetRange.setValues(results)
+    const wpt = new WebPagetest()
+    const results = testIds.map(testId => wpt.getTestResults(testId))
+
+    const targetRange = sheet.getRange(
+      lastCompletedRow + 1,
+      2,
+      results.length,
+      results[0].length,
+    )
+    targetRange.setValues(results)
+  })
 }
