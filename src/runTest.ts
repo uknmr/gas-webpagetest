86e67cd86aadad4874b95ca4749d92160648d3cb
uknmr <y.kinmori@gmail.com>
2018-06-27 15:59:40 +0900

    :bug: Spreadsheet.getActiveSheet() はトリガー実行では存在しないらしい

--- src/runTest.ts
+++ src/runTest.ts
@@ -3,10 +3,11 @@ import WebPagetest = require('./WebPagetest')
 global.runTest = (): void => {
   const key = process.env.WEBPAGETEST_API_KEY
   const url = process.env.RUN_TEST_URL
+  const sheetName = process.env.SHEET_NAME
   const wpt = new WebPagetest(key)
   const testId = wpt.test(url, { runs: 3 })
 
-  const sheet = SpreadsheetApp.getActiveSpreadsheet()!.getActiveSheet()
+  const sheet = SpreadsheetApp.getActiveSpreadsheet()!.getSheetByName(sheetName)
   const lastRow = sheet.getLastRow()
   const targetCell = sheet.getRange(lastRow + 1, 1)
 
