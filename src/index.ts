bc985ad3ea5cccfd3fa6d526aebf4d0c153fdbef
uknmr <y.kinmori@gmail.com>
2018-08-07 00:30:40 +0900

    feat(TimeTrigger): add "setRunTestTimeTriggers" to menu
    - Add `RUN_TEST_INTERVAL_HOURS` to .env
    - Add "Set run test time triggers" to menu
    - Set time-based trigger from scripts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { runTest } from './runTest'
 import { getTestResults } from './getTestResults'
 import { updateColumnTitles } from './updateColumnTitles'
+import { setRunTestTimeTriggers } from './setRunTestTimeTriggers'
 
 function onOpen() {
   const menu = [
     { name: 'Run test', functionName: 'runTest' },
     { name: 'Get test results', functionName: 'getTestResults' },
+    { name: 'Set run test time triggers', functionName: 'setRunTestTimeTriggers' },
     { name: 'Update column titles', functionName: 'updateColumnTitles' },
   ]
   SpreadsheetApp.getActiveSpreadsheet().addMenu('gas-webpagetest', menu)
@@ -14,4 +16,5 @@ function onOpen() {
 global.onOpen = onOpen
 global.runTest = runTest
 global.getTestResults = getTestResults
+global.setRunTestTimeTriggers = setRunTestTimeTriggers
 global.updateColumnTitles = updateColumnTitles
