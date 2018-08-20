879521bb25eb6b248f8a25709cd5f1edbf50c8b4
azu <azuciao@gmail.com>
2018-08-20 19:44:47 +0900

    feat(scripts): Add `create-gas` script

--- scripts/create.js
+++ scripts/create.js
@@ -7,23 +7,31 @@ const rootDir = path.join(__dirname, '..')
 const claspJSONPath = path.join(rootDir, '.clasp.json')
 // run clasp create if have not .clasp.json
 if (!fs.existsSync(claspJSONPath)) {
-  console.log('Create .clasp.json')
   const clasp = require.resolve('.bin/clasp')
   spawn.sync(clasp, ['create'].concat(process.argv.slice(2)), {
     cwd: rootDir,
     stdio: 'inherit',
     shell: true,
   })
+  console.log('✓ Create .clasp.json')
+} else {
+  console.log('⚠ .clasp.json is already exists')
 }
 // modify .clasp.json if it exists
 if (fs.existsSync(claspJSONPath)) {
-  console.log('Add dist directory to .clasp.json')
   const claspJSON = require(claspJSONPath)
-  claspJSON['rootDir'] = 'dist'
-  fs.writeFileSync(claspJSONPath, JSON.stringify(claspJSON, null, 4), 'utf-8')
+  if (!claspJSON['rootDir']) {
+    claspJSON['rootDir'] = 'dist'
+    fs.writeFileSync(claspJSONPath, JSON.stringify(claspJSON, null, 4), 'utf-8')
+    console.log('✓ Add dist directory to .clasp.json')
+  } else {
+    console.log('⚠ .clasp.json already has rootDir')
+  }
 }
 // cp .env.example .env
 if (!fs.existsSync(path.join(rootDir, '.env'))) {
-  console.log('Copy .env.example to .env')
   fs.copyFileSync(path.join(rootDir, '.env.example'), path.join(rootDir, '.env'))
+  console.log('✓ Copy .env.example to .env')
+} else {
+  console.log('⚠ .env is already exists')
 }
