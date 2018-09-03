644582a3704289b530d4831b2fb0a2f62b7aca47
azu <azuciao@gmail.com>
2018-09-03 20:41:19 +0900

    feat(webpagetest): add `WEBPAGETEST_OPTIONS_SCRIPT_PATH` to .env

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,23 @@ const Dotenv = require('dotenv-webpack')
 const GasPlugin = require('gas-webpack-plugin')
 const webpack = require('webpack')
 const fs = require('fs')
-const SCRIPT_FILE_PATH = path.join(__dirname, 'script.txt')
-const WEBPAGETEST_OPTIONS_SCRIPT_CODE = fs.existsSync(SCRIPT_FILE_PATH)
-  ? fs.readFileSync(SCRIPT_FILE_PATH, 'utf-8')
-  : undefined
+const dotenv = require('dotenv')
+const getWebPagetestCode = () => {
+  const result = dotenv.config()
+  if (result.error) {
+    throw result.error
+  }
+  const WEBPAGETEST_OPTIONS_SCRIPT_PATH = result.parsed.WEBPAGETEST_OPTIONS_SCRIPT_PATH
+  if (!WEBPAGETEST_OPTIONS_SCRIPT_PATH) {
+    return
+  }
+  const SCRIPT_FILE_PATH = path.resolve(__dirname, WEBPAGETEST_OPTIONS_SCRIPT_PATH)
+  console.log(SCRIPT_FILE_PATH)
+  if (!fs.existsSync(SCRIPT_FILE_PATH)) {
+    return
+  }
+  return fs.readFileSync(SCRIPT_FILE_PATH, 'utf-8')
+}
 
 module.exports = {
   mode: 'none',
@@ -32,9 +45,7 @@ module.exports = {
   plugins: [
     new Dotenv(),
     new webpack.DefinePlugin({
-      'process.env.WEBPAGETEST_OPTIONS_SCRIPT_CODE': JSON.stringify(
-        WEBPAGETEST_OPTIONS_SCRIPT_CODE
-      ),
+      'process.env.WEBPAGETEST_OPTIONS_SCRIPT_CODE': JSON.stringify(getWebPagetestCode()),
     }),
     new GasPlugin(),
   ],
